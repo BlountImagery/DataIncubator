bf144f76eeb40756d39439ff59dbff59e10564e2
BlountImagery <chris@blountimagery.com>
2017-10-31 12:40:59 +0000

    graph additions

--- project_CTC.py
+++ project_CTC.py
@@ -9,40 +9,19 @@ import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
 
-        
-#load libraries
-from pandas.plotting import scatter_matrix
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+filelocation = "C:\Users\Chris\Desktop\incubator\Project\git\CTC.csv"
 
-   #Load data
-#filelocation = "C:\Users\mbcx9cb4\Downloads\Coefficients-multiples.csv"
-#names = ['co0', 'co1', 'co2', 'co3', 'species']
-#names = ['co0', 'co1', 'co2', 'co3', 'co4', 'co5','co6','co7', 'co8', 'co9', 'co10', 'species']
-
-filelocation = "C:\Users\Chris\Desktop\Data incubator python\Project\CTC.csv"
-
-
-
-names = ['CyclistsNames','TripName','Tagline','No.ofriders','Male/Female','Nationalities','Start','Finish','Duration(months)','Ongoing','Costperperson(originalcurrency)','Costperperson(GBP£)','Totaldistance(km)','Totaldistancemiles','"Averagemonthlydistance(km,calculated)"','Costperkm(calculated)','Costpermonth(calculated)','Typeofbike','Bicyclemake/model','Gears','Handlebars','Brakes','Wheelsize','Luggage','Whatfootwear/pedalsdidyouuse?','Europe','Africa','Australasia','NorthAmerica','SouthAmerica','Asia','PartsofAsia','Round-the-world','Website','Routemap','Kitlist','Titles','Buy(UK)','Buy(World)']
+names = ['CyclistsNames','TripName','Tagline','No.ofriders','Male/Female','Nationalities','Start','Finish','Duration(months)','Ongoing','Costperperson(originalcurrency)','Costperperson(GBP£)','Totaldistancekm','Totaldistancemiles','"Averagemonthlydistance(km,calculated)"','Costperkm','Costpermonth(calculated)','Typeofbike','Bicyclemake/model','Gears','Handlebars','Brakes','Wheelsize','Luggage','Whatfootwear/pedalsdidyouuse?','Europe','Africa','Australasia','NorthAmerica','SouthAmerica','Asia','PartsofAsia','Round-the-world','Website','Routemap','Kitlist','Titles','Buy(UK)','Buy(World)']
 dataset = pd.read_csv(filelocation, names=names)
-  
-    
+      
 def main():   
         
     #print(dataset.shape)
     #print(dataset.head(1))
     #print(dataset.describe())
     #print(dataset.groupby('No. of riders').size())
-        
+
+#Figure 1       
     d1 = {'distance' : dataset.Totaldistancemiles[1:], 'type' : dataset.Typeofbike[1:]}
     df1 = pd.DataFrame(d1)
     df1[['type', 'distance']] = df1[['type', 'distance']].astype(str)
@@ -55,9 +34,21 @@ def main():
     type_comp['dist_per_ride'] = type_comp['distance'] / type_comp['count']
    
     #print type_comp
-    type_comp.plot(y=['dist_per_ride', 'count'], secondary_y=['count'], kind='bar', legend=True)
+    graph = type_comp.plot(y=['dist_per_ride', 'count'], secondary_y=['count'], kind='bar', legend=True)
+    graph.set_xlabel("Bike type")
+    graph.set_ylabel("Distance traveled (miles)")
+    graph.right_ax.set_ylabel("Number of rides per bike type")
+
+#Figure 2 
+    d2 = {'cost' : dataset.Costperkm[1:], 'length' : dataset.Totaldistancekm[1:]}
+    df2 = pd.DataFrame(d2).dropna().astype(float)
     
- 
+    #print df2
+    
+    graph2 = df2.plot(x=['cost'], y=['length'], kind='scatter', legend=True)
+    graph2.set_xlabel("Cost per km traveled")
+    graph2.set_ylabel("Distance traveled (km)")
+       
     
     #Boilplate calling main
 if __name__ == '__main__':
